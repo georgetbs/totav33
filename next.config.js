9770b6e5649e9412a91392d30aacb79daf39eef0
georgetbs <georgekvirkvelia@gmail.com>
2024-08-17 03:26:45 +0400

    bug fixed again

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 const { i18n } = require('./next-i18next.config');
 
 module.exports = {
-  i18n,
+
+    i18n: {
+      locales: ['ka', 'en', 'ru'], // Массив поддерживаемых языков
+      defaultLocale: 'ka', // Язык по умолчанию - грузинский
+      localeDetection: false, // Отключаем автоматическое определение языка
+    },
   webpack: (config, { isServer }) => {
     if (!isServer) {
       config.resolve.fallback.fs = false;
