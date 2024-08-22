756b2171336446048ea16afc7b190b020e9a63ac
georgetbs <georgekvirkvelia@gmail.com>
2024-08-22 19:22:32 +0400

    fonts upgrade fix

--- components/layouts/header.js
+++ components/layouts/header.js
@@ -7,19 +7,9 @@ const Header = () => {
   return (
     <header className="flex flex-col md:flex-row justify-center gap-4 px-5 py-3 items-start">
       <div className="flex flex-col items-center text-center w-full md:w-1/4">
-        <h1
-          className="text-[55px] sm:text-[55px] md:text-[55px] xl:text-[65px] text-green-700 font-medium leading-none"
-          style={{
-            fontFamily: 'SF Compact Rounded',
-          }}
-        >
+        <h1 className="text-[55px] sm:text-[55px] md:text-[55px] xl:text-[65px] text-green-700 font-medium leading-none font-sf-compact">
           TOTA.GE
-          <span
-            className="block text-[13px] sm:text-[13px] md:text-[13px] xl:text-[13px] font-extrabold mt-[5px] sm:mt-[7px] md:mt-[8px] text-red-500"
-            style={{
-              fontFamily: 'Noto Sans Georgian SemiCondensed',
-            }}
-          >
+          <span className="block text-[13px] sm:text-[13px] md:text-[13px] xl:text-[13px] font-extrabold mt-[5px] sm:mt-[7px] md:mt-[8px] text-red-500 font-noto-sans-georgian-semicondensed">
             მთელი საქართველო ერთ სივრცეში
           </span>
         </h1>
